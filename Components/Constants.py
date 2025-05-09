44818dc5438b564a776a2480f6d7856e8ad092e7
Naveen Kumar <nk9363690@gmail.com>
2025-05-09 11:26:21 +0530

    Added the constatns requid in the parsing phase
    This might be necessary in the further steps as well so made it in the global

---
+++ Components/Constants.py
@@ -0,0 +1,8 @@
+# Keywords
+CONDITIONALS = {c: "CONDITIONAL" for c in ["if", "elif", "else"]}
+# Operators
+OPERATORS = {op: "OPERATOR" for op in ["+", "-", "*", "/", "=", "**", "//", "%"]}
+# Relational Operators
+RELOP = {relop: "RELOP" for relop in ['==', '!=', '<', '<=', '>', '>=']}
+# Loops
+LOOPS = {loop: "LOOP" for loop in ['for', 'while']}
