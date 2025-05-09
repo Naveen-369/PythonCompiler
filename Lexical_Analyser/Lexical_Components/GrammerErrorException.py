a4251e0b9f5e910e89e57c3eddf50ad410618fa7
Naveen Kumar <nk9363690@gmail.com>
2025-05-09 11:34:09 +0530

    Refactored the code

---
+++ Lexical_Analyser/Lexical_Components/GrammerErrorException.py
@@ -0,0 +1,12 @@
+from TokenType import TokenType
+
+class GrammerError(Exception): # Inherit the default exception class
+    def __init__(self,Errmessage:str,token:TokenType=None,excepted:TokenType=None):
+        self.token=token
+        self.excepted=excepted
+        error_message=f"Grammer Error : {Errmessage}"
+        if token:
+            error_message += f" | Found : {token.tokenType} : '{token.atr}' at Line - {token.lineNo} Col - {token.colNo}"
+        if excepted:
+            error_message+=f" | Excepted : {excepted}"
+        super().__init__(error_message)
---
+++ Lexical_Analyser/Lexical_Components/Token.py
@@ -0,0 +1,34 @@
+
+from typing import override
+from TokenType import TokenType
+
+class Token:
+    """
+    The Token class classifies the synthesised words from the program into tokens like identifier, constants, etc
+    and stores it in the formate containing its `tokenType`, `value` and `position`.
+    """
+    
+    tokenType:TokenType
+    atr:str
+    lineNo:int
+    colNo:int
+    
+    def __init__(self, tokenType: TokenType, atr: str = None, lineNo: int = None, colNo: int = None) -> None:
+        self.tokenType = tokenType
+        self.atr = atr
+        self.lineNo = lineNo
+        self.colNo = colNo
+    
+    @override
+    def __repr__(self,intendationLevel:int=0) ->str:
+        """This method is for logging during debugging, only for dev team.
+
+        Args:
+            intendationLevel (int, optional): The amount of intendataion of gaps required before the string. Defaults to 0.
+
+        Returns:
+            str: Official String 
+        """
+        return "\t"*intendationLevel+f"{self.tokenType.name} : {self.atr}"
+
+    
